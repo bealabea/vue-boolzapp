89c1d31305b2ead87bb23aae2198c89e575a478e
bealabea <beatricemarin89@yahoo.it>
2022-01-20 20:16:42 +0100

    Commented and clean HTML CSS and JS code

--- js/script.js
+++ js/script.js
@@ -177,6 +177,7 @@ new Vue({
                 ],
             },
         ],
+        // risposte random
         answer: [
                 'Non sono convinto', 'Sei forte', 'Che ne so?', 'Si ma stai calmo!', 'Brindo alla tua!',
                 'Ma chi sei?', 'Buongiorno', 'Raccontami', ':)'  
@@ -185,12 +186,16 @@ new Vue({
         temporaryMex: '',
         searchName: '',
         notifyText: 'Ricevi notifiche di nuovi messaggi',
-        modale: 0,
+        notifyLink: 'Attiva notifiche desktop',
+        modale: null,
     },
     methods: {
+        // con questo metodo, quando clicco su un contatto visualizzo la chat corrispondente (setto currentIndex allo stesso valore dell'indice dei contatti)
         changeChat: function(index) {
             this.currentIndex = index
         },
+        // con questo metodo se la v:modal data all'input della chat è diversa da una stringa vuota pusho la stringa in un oggetto(con data e status)
+        // che automaticamente si visualizzerà come messaggio nella chat, poi svuoto la stringa e compare una risposta random
         sendMessage: function(index){
             if (this.temporaryMex !== ''){
             this.contacts[index].messages.push({ date: this.getDate(), text: this.temporaryMex, status: 'sent'});
@@ -199,37 +204,50 @@ new Vue({
             this.scrollToEnd();
             }
         },
+        // con questo metodo pusho le risposte random dopo un secondo dall'invio del messaggio (inserito in sendMessage)
         answerMessage: function(index) {
             setTimeout(()=>{
                 let rand = Math.floor(Math.random()*this.answer.length);
                 this.contacts[index].messages.push({ date: this.getDate(), text: this.answer[rand], status: 'received'});
              },1000);
         },
+        // con questo metodo porto lo scroll all'ultimo messaggio visualizzato (inserito in sendMessage)
         scrollToEnd: function() {  
             setTimeout(()=>{
             let container = this.$el.querySelector(".chat-bg");
             container.scrollTo(0, container.scrollHeight)
         },1200);
         },
-        // contactResearch: function(element){
-        //     return element.name.toLowerCase().startsWith(this.searchName) || this.searchName === ''; 
-        // },
+        // bonus: ho creato questo metodo per settare la ricezione di notifiche e cambiare icona
         notifyOnOff: function(){
-            if (this.notifyText === 'Ricevi notifiche di nuovi messaggi'){
+            if (this.notifyText === 'Ricevi notifiche di nuovi messaggi' && this.notifyLink === 'Attiva notifiche desktop'){
             this.notifyText = 'Disattiva notifiche di nuovi messaggi';
+            this.notifyLink= 'Disattiva notifiche desktop';
             } else {
             this.notifyText ='Ricevi notifiche di nuovi messaggi';
+            this.notifyLink = 'Attiva notifiche desktop';
             }
         },
+        // con questo metodo e la libreria dayJs creo ora e data dinamici, (inserito in sendMessage e answerMessage)
         getDate() {
             return dayjs().format('DD/MM/YYYY  HH:mm:ss')
         },
+        // con questo metodo creo una funzione che individua l'ultimo messaggio della chat
+        // (utilizzato per aggiungere dinamicamente il giorno e l'ora dell'ultimo accesso del contatto)
         lastAccess: function(index) {  
             return this.contacts[index].messages.length -1;
         },
+        // con questo metodo setto la modale in false se è uguale all'indice del messaggio, quindi se aperta (= all'indice) si chiude, se è chiusa (=false) si apre
         openModal(index){
-            this.modale = index;
-            
+            if(this.modale === index){
+                this.modale = false;
+            } else {
+                this.modale = index;
+            }
         }
     }
 });
+
+        // contactResearch: function(element){
+        //     return element.name.toLowerCase().startsWith(this.searchName) || this.searchName === ''; 
+        // },
